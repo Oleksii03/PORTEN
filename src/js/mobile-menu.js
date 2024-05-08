665b826651d6652ce2f9b5af3c6538b6ed89a948
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2024-05-08 14:41:07 +0300

    add

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,9 +2,14 @@ const openMenuBtn = document.querySelector("[data-menu-open]");
 const closeMenuBtn = document.querySelector("[data-menu-close]");
 const menu = document.querySelector("[data-menu]");
 
+const backdrop = document.querySelector("[data-backdrop]");
+
+console.log(backdrop);
+
 openMenuBtn.addEventListener("click", toggleMenu);
 closeMenuBtn.addEventListener("click", toggleMenu);
 
 function toggleMenu () {
   menu.classList.toggle("is-hidden");
+  backdrop.classList.toggle("backdrop--hidden");
 }

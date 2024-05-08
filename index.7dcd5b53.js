38ed64013891b2a0adb1044c1550027f584ab844
Oleksii03 <102686871+Oleksii03@users.noreply.github.com>
2024-05-08 11:41:28 +0000

    Deploying to gh-pages from @ Oleksii03/PORTEN@665b826651d6652ce2f9b5af3c6538b6ed89a948 🚀

--- index.7dcd5b53.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("[data-menu-open]"),t=document.querySelector("[data-menu-close]"),n=document.querySelector("[data-menu]");function c(){n.classList.toggle("is-hidden")}e.addEventListener("click",c),t.addEventListener("click",c);
-//# sourceMappingURL=index.7dcd5b53.js.map
--- index.9364162e.js
+++
@@ -1,2 +0,0 @@
-!function(){const e=document.querySelector("[data-menu-open]"),t=document.querySelector("[data-menu-close]"),n=document.querySelector("[data-menu]");function c(){n.classList.toggle("is-hidden")}e.addEventListener("click",c),t.addEventListener("click",c)}();
-//# sourceMappingURL=index.9364162e.js.map

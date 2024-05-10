ecbafa91bec21bc3bdc27b08f6dca3c11539593d
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2024-05-10 17:38:42 +0300

    Update slider.js

--- src/js/slider.js
+++ src/js/slider.js
@@ -15,7 +15,15 @@ let count = 1;
 countText.textContent = count;
 
 document.querySelector('.clocks-list__btn-next').addEventListener('click', (e) => {
+
+  if (count >= totalItem) {
+    e.currentTarget.disabled = true;
+    e.currentTarget.style.opacity = 0.3;
+    return;
+  }
+
   count += 1;
+
   width += 282;
   sliderLine.style.left = -width + 'px';
   countText.textContent = count;

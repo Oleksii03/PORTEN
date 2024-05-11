7800da9dcfb9484d97b4ce076dfce3e3699120c7
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2024-05-11 21:49:33 +0300

    finish-new-arrivals

--- src/js/slider.js
+++ src/js/slider.js
@@ -7,17 +7,45 @@ const countText = document.querySelector('.js-count');
 const btnPrev = document.querySelector('.clocks-list__btn-prev');
 const btnNext = document.querySelector('.clocks-list__btn-next');
 
-// ------------------------------------
+// -----------/-refs------------------------
 
-const offset = sliderLine.scrollWidth / items.length;
-const totalItem = items.length - 2;
+let width = 0;
+let count = 0;
+let offset = 0;
+
+const totalItem = items.length - 1;
 
 totalText.textContent = totalItem;
 
-let width = 0;
-let count = 1;
+window.addEventListener('resize', init);
 
-countText.textContent = count;
+function init () {
+  const container = document.querySelector('.cont-r').offsetWidth;
+
+  width = 0;
+
+  if (container < 760) {
+    offset = sliderLine.scrollWidth / items.length;
+    console.log(offset);
+  } else {
+    offset = sliderLine.scrollWidth / (items.length - 1);
+    console.log(offset);
+  }
+
+  sliderLine.style.left = 0 + 'px';
+  count = 1;
+
+  if (count <= 1) {
+    btnPrev.disabled = true;
+    btnPrev.style.opacity = 0.3;
+    btnNext.disabled = false;
+    btnNext.style.opacity = 1;
+    countText.textContent = count;
+    return;
+  }
+}
+
+init();
 
 
 btnNext.addEventListener('click', increment);
@@ -30,7 +58,9 @@ function increment (e) {
   sliderLine.style.left = -width + 'px';
   countText.textContent = count;
 
-  if (count >= items.length - 2) {
+  console.log(count);
+
+  if (count >= items.length - 1) {
     btnNext.disabled = true;
     btnNext.style.opacity = 0.3;
     return;

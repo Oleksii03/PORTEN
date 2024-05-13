b3eb26b5a42aff2abd8792e31803ac4d3219266d
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2024-05-13 15:18:01 +0300

    add-accordion-js

---
+++ src/js/accordion.js
@@ -0,0 +1,21 @@
+const accordion = document.querySelector('.accordion');
+
+accordion.addEventListener('click', onOpenAccordionClick);
+
+function onOpenAccordionClick (e) {
+  if (!e.target.classList.contains('label')) return;
+
+  let correntBox = e.target.closest('.box');
+  let correntContent = e.target.nextElementSibling;
+
+  correntBox.classList.toggle('active');
+
+  if (correntBox.classList.contains('active')) {
+    correntContent.style.maxHeight = correntContent.scrollHeight + 'px';
+  } else {
+    correntContent.style.maxHeight = 0;
+  }
+}
+
+
+
